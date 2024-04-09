808198444d353a45abc29f45fb5f159655842591
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-04-09 12:43:07 +0200

    Collegamento del movie_db

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -14,6 +15,17 @@ class PageController extends Controller
     //funzione per mostarre i movies
 
     public function showMovie() {
-        return view('movie');
+
+        // vado a ceracre i film della tabella
+        //e con il metodo all() prendo tutte le righe della tabella del mio database
+        // che ho collegato al Model
+
+        $movies = Movie::all();
+
+        dd($movies);
+
+        
+        return view('movie',);
+
     }
 }
