0d6c8f416abab9ea05ca1ac70177b8eb9020a0b7
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-04-09 11:56:43 +0200

    collegamento seconda pagina

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers\Guest;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -10,5 +9,11 @@ class PageController extends Controller
     // creo una funzione (index) in cui inserisco al logica 
     public function index() {
         return view('home');
+    }
+
+    //funzione per mostarre i movies
+
+    public function showMovie() {
+        return view('movie');
     }
 }
---
+++ resources/views/movie.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts/app')
+
+@section('content')
+
+<div class="container py-5">
+    Pagina dei movie
+</div>
+
+
+@endsection
