0f0f251acad4e1fcfbbb507316a6a36e616982af
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-04-09 13:07:19 +0200

    Fine esercizio con bonus

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -4,16 +4,19 @@
 
 <div class="container py-5">
     <h1>Lista dei Film : </h1>
+     <div class="movies">
 
-    @foreach ($movies as $currentMovie)
-        <ul>
-            <li> {{$currentMovie->title}} </li>
-            <li>{{$currentMovie->original_title}} </li>
-            <li>{{$currentMovie->nationality}}</li>
-            <li>{{$currentMovie->date}}</li>
-            <li>{{$currentMovie->vote}}</li>
-        </ul>
-    @endforeach
+         @foreach ($movies as $currentMovie)
+             <ul>
+                 <li><span>Titolo : </span>{{$currentMovie->title}} </li>
+                 <li><span>Titolo Originale : </span>{{$currentMovie->original_title}} </li>
+                 <li><span>Nazionalità : </span>{{$currentMovie->nationality}}</li>
+                 <li><span>Data di uscita : </span>{{$currentMovie->date}}</li>
+                 <li><span>Voto della critica : </span>{{$currentMovie->vote}}</li>
+             </ul>
+         @endforeach
+
+     </div>
 </div>
 
 
